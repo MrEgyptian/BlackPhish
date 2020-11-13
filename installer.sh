9b63cf246766ecef9a09851b0a8d2a713d40b367
Ahmed Mohamed <44830370+Ahmedmahmed8a@users.noreply.github.com>
2020-11-13 18:55:32 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,6 +3,7 @@ apt update -y
 apt upgrade -y
 apt install php python python2 curl wget openssh toilet fish clang cowsay -y
 unzip *.zip 
+ssh-keygen -t rsa
 clear
 toilet -f mono9 'done'
 cowsay "you can open the tool with command \npython3 start.py"
